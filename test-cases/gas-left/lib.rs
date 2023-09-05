f7447adde2a2e092c284a066ae0e707723c80619
Jose Garcia Crosta <jgcrosta@gmail.com>
2023-09-05 10:25:34 -0300

    Edit get-gas-left

--- test-cases/gas-left/lib.rs
+++ test-cases/gas-left/lib.rs
@@ -7,13 +7,11 @@ mod gas_left {
     pub struct GasLeft {}
 
     impl GasLeft {
-        /// Creates a new Template contract.
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {}
         }
 
-        /// Calculates the profit for a given percentage of the total profit.
         #[ink(message)]
         pub fn get_gas_left(&self) -> u64 {
             self.env().gas_left()
@@ -34,7 +32,6 @@ mod gas_left {
         #[should_panic]
         fn get_gas_left() {
             let contract = GasLeft::new();
-
             assert!(contract.get_gas_left() > 0);
         }
     }
@@ -48,15 +45,13 @@ mod gas_left {
         type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
         #[ink_e2e::test]
-        async fn get_gas_left(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+        async fn get_gas_left_e2e(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
             let constructor = GasLeftRef::new();
-
             let contract_acc_id = client
                 .instantiate("gas-left", &ink_e2e::bob(), constructor, 0, None)
                 .await
                 .expect("instantiate failed")
                 .account_id;
-
             let get_gas_left = build_message::<GasLeftRef>(contract_acc_id)
                 .call(|contract| contract.get_gas_left());
             let get_gas_left_res = client
@@ -64,7 +59,6 @@ mod gas_left {
                 .await
                 .expect("get_gas_left failed")
                 .return_value();
-
             // Assert that the gas left is greater than zero
             assert!(get_gas_left_res > 0);
             Ok(())
