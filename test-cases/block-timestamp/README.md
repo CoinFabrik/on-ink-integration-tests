7b2d38989ef6d00de2de84e219d26ed4b0d0bd29
Brian Ramirez <ramirez7358@gmail.com>
2023-09-19 11:02:07 -0300

    Tests and documentation complete

---
+++ test-cases/block-timestamp/README.md
@@ -0,0 +1,35 @@
+# Function `block_timestamp`
+
+```rust
+pub fn block_timestamp<E>() -> E::Timestamp
+where
+    E: Environment,
+```
+
+## Description
+
+`block_timestamp` returns the current block timestamp.
+
+## Related ink! functions
+
+- [block_timestamp](https://paritytech.github.io/ink/ink_env/fn.block_timestamp.html)
+- [advance_block](https://paritytech.github.io/ink/ink_env/test/fn.advance_block.html)
+
+## Test case
+
+The test asks the contract for the block_timestamp at two different times and then compares them to verify that the first is less than the second.
+
+| \#  | Test            | Integration | E2E |
+| --- | --------------- | :---------: | :-: |
+| 1   | Attempts to check that the timestamp is advancing |     ✅      | ✅  |
+
+## Comparison Integration vs E2E
+
+The `block_timestamp` function works in both cases. However, in integration test you have to advance the block manually to get a different value.
+
+## Result
+
+- Can it be implemented?
+- How long will it take?
+- Ideas for implementation
+- Other notes
---
+++ test-cases/block-timestamp/lib.rs
@@ -0,0 +1,77 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod block_timestamp {
+
+    #[ink(storage)]
+    pub struct BlockTimestamp {}
+
+    impl BlockTimestamp {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message)]
+        pub fn get_block_timestamp(&self) -> u64 {
+            self.env().block_timestamp()
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use ink::env::{test::advance_block, DefaultEnvironment};
+
+        use super::*;
+
+        #[ink::test]
+        fn split_profit_works() {
+            let contract = BlockTimestamp::new();
+
+            let timestamp = contract.get_block_timestamp();
+            advance_block::<DefaultEnvironment>();
+
+            let timestamp_2 = contract.get_block_timestamp();
+            advance_block::<DefaultEnvironment>();
+
+            assert!(timestamp < timestamp_2);
+        }
+    }
+
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        use ink_e2e::build_message;
+
+        use super::*;
+
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+        #[ink_e2e::test]
+        async fn split_profit_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let constructor = BlockTimestampRef::new();
+
+            let contract_acc_id = client
+                .instantiate("block_timestamp", &ink_e2e::bob(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let timestamp = build_message::<BlockTimestampRef>(contract_acc_id.clone())
+                .call(|contract| contract.get_block_timestamp());
+
+            let timestamp_res = client
+                .call(&ink_e2e::bob(), timestamp.clone(), 0, None)
+                .await
+                .expect("block_timestamp failed");
+
+            let timestamp_res_2 = client
+                .call(&ink_e2e::bob(), timestamp, 0, None)
+                .await
+                .expect("block_timestamp failed");
+
+            assert!(timestamp_res.return_value() < timestamp_res_2.return_value());
+
+            Ok(())
+        }
+    }
+}
