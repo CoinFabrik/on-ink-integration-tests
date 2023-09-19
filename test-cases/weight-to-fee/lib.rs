f0f61cf0323a9fa0ebec99edf17e63e84f02bc65
Jose Garcia Crosta <jgcrosta@gmail.com>
2023-09-19 11:29:23 -0300

    Minor changes

--- test-cases/weight-to-fee/lib.rs
+++ test-cases/weight-to-fee/lib.rs
@@ -3,25 +3,29 @@
 #[ink::contract]
 mod weight_to_fee {
 
+    // Fixed gas amount
+    const _GAS_AMOUNT: u64 = 5000;
+
     #[ink(storage)]
     pub struct WeightToFee {}
 
     impl WeightToFee {
-        /// Creates a new WeightToFee contract.
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {}
         }
 
-        /// Calculates the price of a specified amount of gas.
         #[ink(message)]
         pub fn weight_to_fee(&self, gas: u64) -> Balance {
             self.env().weight_to_fee(gas)
         }
     }
 
-    /// Fixed amount of gas for both test environments.
-    const _GAS_AMOUNT: u64 = 5000;
+    impl Default for WeightToFee {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
 
     #[cfg(test)]
     mod tests {
@@ -29,18 +33,17 @@ mod weight_to_fee {
 
         #[ink::test]
         fn weight_to_fee_works() {
+            // Given
             let contract = WeightToFee::new();
-            let gas: u64 = _GAS_AMOUNT;
 
-            let gas_price = contract.weight_to_fee(gas);
+            // When
+            let gas_price = contract.weight_to_fee(_GAS_AMOUNT);
 
-            // TODO:
-            // assert!(gas_cost > 0);
-
+            // Then
             println!("Gas price in Integration: {:?}", gas_price);
             println!(
                 "Gas price per unit in Integration: {:?}",
-                gas_price / <u64 as Into<u128>>::into(gas)
+                gas_price / <u64 as Into<u128>>::into(_GAS_AMOUNT)
             );
         }
     }
@@ -54,8 +57,8 @@ mod weight_to_fee {
 
         #[ink_e2e::test]
         async fn weight_to_fee_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Given
             let constructor = WeightToFeeRef::new();
-            let gas: u64 = _GAS_AMOUNT;
 
             let contract_acc_id = client
                 .instantiate("weight-to-fee", &ink_e2e::alice(), constructor, 0, None)
@@ -63,8 +66,9 @@ mod weight_to_fee {
                 .expect("instantiate failed")
                 .account_id;
 
+            // When
             let weight_to_fee = build_message::<WeightToFeeRef>(contract_acc_id.clone())
-                .call(|contract| contract.weight_to_fee(gas));
+                .call(|contract| contract.weight_to_fee(_GAS_AMOUNT));
 
             let gas_price = client
                 .call(&ink_e2e::bob(), weight_to_fee, 0, None)
@@ -72,13 +76,11 @@ mod weight_to_fee {
                 .expect("split_profit failed")
                 .return_value();
 
-            // TODO:
-            // assert!(gas_cost > 0);
-
+            // Then
             println!("Gas price in E2E: {:?}", gas_price);
             println!(
                 "Gas price per unit in E2E: {:?}",
-                gas_price / <u64 as Into<u128>>::into(gas)
+                gas_price / <u64 as Into<u128>>::into(_GAS_AMOUNT)
             );
 
             Ok(())
