c4538981aed107752a2edb3395045a0972698c12
Facundo Lerena <faculerena@gmail.com>
2023-11-14 13:05:07 -0300

    Added default balances test

--- test-cases/balance/lib.rs
+++ test-cases/balance/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-
 #[ink::contract]
 mod get_balance {
 
@@ -25,12 +24,12 @@ mod get_balance {
         }
     }
 
-
     #[cfg(test)]
     const INITIAL_BALANCE_INTEGRATION: u128 = 1000000;
     #[cfg(all(test, feature = "e2e-tests"))]
     const INITIAL_BALANCE_E2E: u128 = 1000000000;
-
+    #[cfg(all(test, feature = "e2e-tests"))]
+    const CORRECTED_BALANCE_INTEGRATION: u128 = 1000000000;
 
     #[cfg(all(test, feature = "e2e-tests"))]
     mod e2e_tests {
@@ -41,13 +40,7 @@ mod get_balance {
             // Given
             let constructor = GetBalanceRef::new();
             let contract_to_call_acc_id = client
-                .instantiate(
-                    "get_balance",
-                    &ink_e2e::alice(),
-                    constructor,
-                    0,
-                    None,
-                )
+                .instantiate("get_balance", &ink_e2e::alice(), constructor, 0, None)
                 .await
                 .expect("instantiate failed")
                 .account_id;
@@ -58,16 +51,71 @@ mod get_balance {
                 .await
                 .expect("Failed to get account balance");
 
-
             // Then
-            assert_eq!(
-                contract_balance_before,
-                INITIAL_BALANCE_E2E
-            );
+            assert_eq!(contract_balance_before, INITIAL_BALANCE_E2E);
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn default_balances(mut client: Client<C, E>) -> E2EResult<()> {
+            //get the balance from alice, bob and all the default accounts
+
+            let alice_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Alice))
+                .await
+                .expect("Failed to get account balance");
+
+            let bob_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Bob))
+                .await
+                .expect("Failed to get account balance");
+
+            let charlie_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Charlie))
+                .await
+                .expect("Failed to get account balance");
+
+            let dave_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Dave))
+                .await
+                .expect("Failed to get account balance");
+
+            let eve_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Eve))
+                .await
+                .expect("Failed to get account balance");
+
+            let ferdie_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Ferdie))
+                .await
+                .expect("Failed to get account balance");
+
+            let one_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::One))
+                .await
+                .expect("Failed to get account balance");
+
+            let two_balance_before = client
+                .balance(ink_e2e::account_id(ink_e2e::AccountKeyring::Two))
+                .await
+                .expect("Failed to get account balance");
+
+            let arr = [
+                alice_balance_before,
+                bob_balance_before,
+                charlie_balance_before,
+                dave_balance_before,
+                eve_balance_before,
+                ferdie_balance_before,
+                one_balance_before,
+                two_balance_before,
+            ];
+
+            assert_eq!(arr, [CORRECTED_BALANCE_INTEGRATION; 8]);
+
             Ok(())
         }
     }
-
 
     #[cfg(test)]
     mod tests {
@@ -90,5 +138,4 @@ mod get_balance {
             assert_eq!(INITIAL_BALANCE_E2E, INITIAL_BALANCE_INTEGRATION);
         }
     }
-
 }
